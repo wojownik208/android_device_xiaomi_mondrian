a9cb42e9c5aefb9494930fff1b2e4f56b92e382a
keosh <conquer2152016@gmail.com>
2025-04-13 13:07:46 +0000

    mondrian: Add MiuiCamera support

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/mondrian/mondrian-vendor.mk)
 
+# Inherit from MiuiCamera setup
+$(call inherit-product, device/xiaomi/miuicamera-mondrian/device.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     firmware_aw_cali.bin_symlink
